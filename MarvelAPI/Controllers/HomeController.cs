3be49ef3cf3338460b7bd37b0a70f2c1af9f49d0
Vinicius Nogueira <nogueirav@outlook.com>
2017-08-02 21:33:46 -0300

    Carregando listagem com personagens

--- MarvelAPI/Controllers/HomeController.cs
+++ MarvelAPI/Controllers/HomeController.cs
@@ -1,14 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using System.Net.Http;
 using System.Text;
 using System.Security.Cryptography;
 using Newtonsoft.Json;
-using Microsoft.Extensions.Configuration;
-
 
 namespace MarvelAPI.Controllers
 {
@@ -18,60 +14,54 @@ namespace MarvelAPI.Controllers
         {
             Personagem personagem;
 
-            using (var client = new HttpClient())
+            HttpClient client = new HttpClient();
+            
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            string ts = DateTime.Now.Ticks.ToString();
+            string publicKey = "PUBLICKEY";
+            string privateKey = "PRIVATEKEY";
+            string hash = GerarHash(ts, publicKey, privateKey);
+
+            //VALIDAR SE O REQUEST ESTÁ SENDO MONTADO CORRETAMENTE
+            //                string teste = $"http://gateway.marvel.com/v1/public/" +
+            //    $"characters?ts={ts}&apikey={publicKey}&hash={hash}&" +
+            //$"name={Uri.EscapeUriString("Captain America")}";
+
+
+            HttpResponseMessage response = client.GetAsync($"http://gateway.marvel.com/v1/public/" +
+                                                           $"characters?ts={ts}&apikey={publicKey}&hash={hash}&limit=100").Result;
+
+            response.EnsureSuccessStatusCode();
+
+            string conteudo = response.Content.ReadAsStringAsync().Result;
+
+            dynamic resultado = JsonConvert.DeserializeObject(conteudo);
+            
+            //EXEMPLO DE COMO PEGAR OS DADOS DO JSON
+            //personagem.Nome = resultado.data.results[1].name;
+            //personagem.UrlImagem = resultado.data.results[1].thumbnail.path;
+
+            List<Personagem> Personagens = new List<Personagem>();
+
+            foreach (var item in resultado.data.results)
             {
-                client.DefaultRequestHeaders.Accept.Clear();
-                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                personagem = new Personagem()
+                {
+                    Nome = item.name,
+                    Descricao = item.description,
+                    UrlImagem = item.thumbnail.path + "." +
+                item.thumbnail.extension,
+                    UrlWiki = item.urls[1].url
+                };
 
-                string ts = DateTime.Now.Ticks.ToString();
-                string publicKey = "PUBLICKEY";
-                string privateKey = "PRIVATEKEY";
-                string hash = GerarHash(ts, publicKey, privateKey);
-//                string teste = $"http://gateway.marvel.com/v1/public/" +
-//    $"characters?ts={ts}&apikey={publicKey}&hash={hash}&" +
-//$"name={Uri.EscapeUriString("Captain America")}";
+                    Personagens.Add(personagem);
 
-
-                HttpResponseMessage response = client.GetAsync($"http://gateway.marvel.com/v1/public/" +
-                                                               $"characters?ts={ts}&apikey={publicKey}&hash={hash}&" +
-                                                               $"name={Uri.EscapeUriString("Wolverine")}").Result;
-
-                response.EnsureSuccessStatusCode();
-
-                string conteudo = response.Content.ReadAsStringAsync().Result;
-
-                dynamic resultado = JsonConvert.DeserializeObject(conteudo);
-
-                personagem = new Personagem();
-
-                personagem.Nome = resultado.data.results[0].name;
-                personagem.Descricao = resultado.data.results[0].description;
-                personagem.UrlImagem = resultado.data.results[0].thumbnail.path + "." +
-                                       resultado.data.results[0].thumbnail.extension;
-                personagem.UrlWiki = resultado.data.results[0].urls[1].url;
-
-            }
+                }
 
 
-            return View(personagem);
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
+            ViewBag.Personagens = Personagens;
             return View();
         }
 
